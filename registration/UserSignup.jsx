dc7359027258af81790046921aad99bbc4c2ca6a
KratiGupta <kratigupta8545@gmail.com>
2021-10-10 18:37:18 +0530

    [Krati]Refactored:Jsx Code For Signup Page

--- registration/UserSignup.jsx
+++ registration/UserSignup.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import logo from '../Assets/logo.png'
+import accountlogo from '../Assets/account-logo.svg'
 import TextField from '@mui/material/TextField';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 
 
 function UserSignup() {
     return (
+        <div className = "body">
         <div className = "Main">
+            <div className = "left-sec">
             <img src = {logo} className = "applogo"></img>
             <div className="heading">Create Your Fundoo Account</div>
             <div className="subhead">Continue to fundoo</div>
+
             <div className='name'>
                 <TextField id="firstname" label="First name" variant="outlined" size='small' />
                 <TextField id="lastname" label="Last name" variant="outlined" size='small'/>
             </div>
+
             <div className = "User-Name">
-            <TextField id="user" label="Username" variant="outlined" size='small' value='@gmail.com' />
+            <TextField style={{ width : '25vw'}} id="user" label="Username" variant="outlined" size='small'  value='@gmail.com' />
             <div className='user-text'>You can use letters, numbers & periods</div>
 
             </div>
@@ -25,9 +31,11 @@ function UserSignup() {
                 <TextField id="password" label="Password" variant="outlined" size='small' />
                 <TextField id="confirm" label="Confirm" variant="outlined" size='small' />
             </div>
+
             <div className='bottom-text'>
                 Use 8 or more characters with a mix of letters, numbers & symbols
             </div>
+
             <div className = 'checkbox'>
             <FormGroup>
             <FormControlLabel control={<Checkbox defaultChecked />} label="Show password" />
@@ -36,10 +44,27 @@ function UserSignup() {
 
             <div className="last-sec">
                 <div className="text">Sign in instead</div>
-                <button className="Next-button">Next</button>
+                <Button className="Next-button" variant = "contained">Next</Button>
+            </div>
+            </div>
+            
+            <div className="right-sec"> 
+            <img src = {accountlogo} className ="account-logo"></img>
+            <div className = "acclogo-text">  One account. All of Fundoo working for you.</div> 
             </div>
             
         </div>
+        <div className="footer">
+            <div className="left-foot">
+
+            </div>
+            <div className = "right-foot">
+                <div>Help      Privacy      Terms</div>
+            </div>
+        </div>
+        </div>
+
+        
     )
 }
 
