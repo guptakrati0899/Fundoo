57cfbec8c9fe331d7cbf0c01902f3851e6de9c7a
KratiGupta <kratigupta8545@gmail.com>
2021-10-12 17:51:39 +0530

    [Krati] Refactored: App.js Code for adding Routing

--- App.js
+++ App.js
@@ -3,12 +3,36 @@ import './App.css';
 import UserSignup from './pages/registration/UserSignup';
 import Forgotemail from './pages/Forgot_email/Forgotemail'
 import Signin from './pages/Sign_in/Signin';
+import Resetpassword from './pages/Reset_password/Resetpassword';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
 
 function App() {
   return <div className="AppDiv">
+    <Router>
+    <Switch>
+      <Route exact path="/" component={Signin} />
+
+
+      <Route exact path="/resetpassword" component={Resetpassword} />
+
+      <Route exact path="/UserSignup" component={UserSignup} />
+
+
+      <Route exact path="/Forgotemail" component={Forgotemail} />
+      </Switch>
+      </Router>
      {/* <UserSignup/>  */}
-      {/* <Signin/>   */}
-    <Forgotemail/>  
+      {/* <Signin/>    */}
+    {/* <Forgotemail/>   */}
+    {/* <Resetpassword/> */}
     </div>
 }
 
