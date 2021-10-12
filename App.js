b2dd6c1968b66dbae327259af5efa3205665e6fc
KratiGupta <kratigupta8545@gmail.com>
2021-10-12 13:13:52 +0530

    [Krati]Refactored: Code For Pages

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import UserSignup from './registration/UserSignup';
+import UserSignup from './pages/registration/UserSignup';
+import Forgotemail from './pages/Forgot_email/Forgotemail'
+import Signin from './pages/Sign_in/Signin';
 
 function App() {
-  return <div className="AppDiv"><UserSignup/></div>
+  return <div className="AppDiv">
+     {/* <UserSignup/>  */}
+      {/* <Signin/>   */}
+    <Forgotemail/>  
+    </div>
 }
 
 export default App;
