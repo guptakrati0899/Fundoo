4557d9567cbbcd8e42a282a136ab559483c66031
KratiGupta <kratigupta8545@gmail.com>
2021-10-12 12:51:36 +0530

    [Krati]Refactored:Code For Forgot Email Page

---
+++ pages/Forgot_email/Forgotemail.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './Forgotemail.css'
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+
+
+function Forgotpassword() {
+    return (
+  
+        <div className = "header">
+    
+            <div className = "fundoo-logo">
+                <p className = "first">F</p>
+                <p className = "second">u</p>
+                <p className = "third">n</p>
+                <p className = "fourth">d</p>
+                <p className = "fifth">o</p>
+                <p className = "sixth">o</p>
+            </div>
+
+            <div className="header-text">Find your email</div>
+            <div className="subheader-text">Enter your phone number or recovery email</div>
+
+            <div className='phonenumber-email'>
+                <TextField fullWidth id="phonenumber-email" label="Phone number or email" variant="outlined" size='large' />
+            </div>
+
+            <div className="end">
+                <Button className="Next-button" variant = "contained">Next</Button>
+            </div>
+
+            </div>
+            
+            )
+            }
+
+            export default Forgotpassword
