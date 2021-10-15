cdd3e14b9f86bf08f2282e45cf747986dbe47c5c
KratiGupta <kratigupta8545@gmail.com>
2021-10-15 23:27:43 +0530

    [Krati]Refactored: Code for axios service of forgot mail

--- service/axiosforgotemail.js
+++ service/axiosforgotemail.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const forgot = async function(obj) {
-    let response = await axios.post("http://fundoonotes.incubation.bridgelabz.com/api/user/23/verify", obj);
+    let response = await axios.post("", obj);
     return response;
 }
 
