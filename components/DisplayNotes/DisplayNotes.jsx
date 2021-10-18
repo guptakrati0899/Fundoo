ead179b8951fc179a0a477c6c40accbce58dd1a3
KratiGupta <kratigupta8545@gmail.com>
2021-10-18 23:05:34 +0530

    [Krati]Added: Code for Display Notes Component using class component

---
+++ components/DisplayNotes/DisplayNotes.jsx
@@ -0,0 +1,32 @@
+
+import "../DisplayNotes/DisplayNotes.css"
+
+import React, { Component } from 'react'
+
+export class DisplayNotes extends Component {
+  render() {
+    return (
+      <div>
+         <div className="note1">
+            <div className="note1_content">
+              <h4>To Do</h4>
+              <div className="content1">Assignment</div>
+              <div className="content2">Pay cc bills</div>
+            </div>
+      </div> 
+      <div className="note2">
+            <div className="note2_content">
+              <h4>Schedule</h4>
+              <div className="content-1">Wakeup at 7am</div>
+              <div className="content-2">Sleep at 1am</div>
+            </div>
+      </div> 
+        
+      </div>
+    )
+  }
+}
+
+export default DisplayNotes
+
+
