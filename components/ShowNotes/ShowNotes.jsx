327728e3e31ceaca8cf09b59ab9ad296ca898c54
KratiGupta <kratigupta8545@gmail.com>
2021-10-19 19:28:07 +0530

    [Krati]Refactored: Code for  Show Notes to display created notes on screen by using get method

--- components/ShowNotes/ShowNotes.jsx
+++ components/ShowNotes/ShowNotes.jsx
@@ -19,11 +19,6 @@ const ShowNotes = (props) => {
           {" "}
           <div className="note2-title">
           <h3>{props.info.title}</h3>
-          <img
-              id="notes-pin"
-              src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICA8cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+CiAgPHBhdGggZmlsbD0iIzAwMCIgZD0iTTE3IDR2N2wyIDN2MmgtNnY1bC0xIDEtMS0xdi01SDV2LTJsMi0zVjRjMC0xLjEuOS0yIDItMmg2YzEuMTEgMCAyIC44OSAyIDJ6TTkgNHY3Ljc1TDcuNSAxNGg5TDE1IDExLjc1VjRIOXoiLz4KPC9zdmc+Cg=="
-              alt="pin-pic-display"
-            />
           </div>
           <div className="note2-paragraph"> {props.info.description}</div>
  
