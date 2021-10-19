58a12ad7495ef5bd56ef9fc7ac393a17e209f4d3
KratiGupta <kratigupta8545@gmail.com>
2021-10-19 19:19:07 +0530

    [Krati]Refactored: Code for  Display Notes by using get method

--- components/DisplayNotes/DisplayNotes.jsx
+++ components/DisplayNotes/DisplayNotes.jsx
@@ -1,32 +1,98 @@
+import React, { useEffect } from "react";
+ import "./DisplayNotes.css"
+import UserServices from '../../service/userservice';
+import ShowNotes from "../ShowNotes/ShowNotes";
+// import Notes from "../Notes/Notes";
 
-import "./DisplayNotes.css"
+// import React, { Component } from 'react'
 
-import React, { Component } from 'react'
 
-export class DisplayNotes extends Component {
-  render() {
-    return (
-      <div>
-         <div className="note1">
-            <div className="note1_content">
-              <h4>To Do</h4>
-              <div className="content1">Assignment</div>
-              <div className="content2">Pay cc bills</div>
-            </div>
-      </div> 
-      <div className="note2">
-            <div className="note2_content">
-              <h4>Schedule</h4>
-              <div className="content-1">Wakeup at 7am</div>
-              <div className="content-2">Sleep at 1am</div>
-            </div>
-      </div> 
+ const obj = new UserServices();
+
+
+
+
+
+// export class DisplayNotes extends Component {
+
+
+//   constructor(props) {
+//     super(props)
+  
+//     this.state = {
+
+
+       
+//     }
+
+//   }
+ 
+  
+  
+
+
+  
+//   render() {
+//     // console.log(this.props.value.description)
+//     return (
+//       <div>
         
-      </div>
-    )
-  }
-}
+        
+//             {/* <div className="disp-note">
+//               {this.state.notes.map(this.note)}
+//             </div>
+//                 <div className="note">
+//                 <div>
+//                     {this.props.value.title}
+//                 </div>
+//                     {this.props.value.description}
+//                 </div>
+//             </div> */}
+        
+//          {/* <div className="note1">
+//             <div className="note1_content">
+//               <h4>To Do</h4>
+//               <div className="content1">Assignment</div>
+//               <div className="content2">Pay cc bills</div>
+//             </div>
+//       </div> 
+//       <div className="note2">
+//             <div className="note2_content">
+//               <h4>Schedule</h4>
+//               <div className="content-1">Wakeup at 7am</div>
+//               <div className="content-2">Sleep at 1am</div>
+//             </div>
+//       </div>  */}
+        
+//       </div>
+//     )
+//   }
+// }
 
-export default DisplayNotes
+// export default DisplayNotes
 
 
+
+// import Notes from "./Notes";
+
+
+const DisplayNotes = () => {
+  const [noteArray, setArray] = React.useState([]);
+  useEffect(() => {
+    obj.getAllNotes()
+      .then((response) => {
+        setArray(response.data.data.data);
+        console.log(noteArray);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(noteArray);
+
+   const noteList = noteArray.map((info) => <ShowNotes info={info} />);
+
+   return <div className="displaynotenew-mainContainer">{noteList}</div>;
+};
+export default DisplayNotes;
